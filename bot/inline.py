28c45b2d37ca88a5105c0570bef350e82ec71373
Umid977222 <105707758+Umid977222@users.noreply.github.com>
2022-10-02 23:15:17 +0500

    calendar ready but state problem

--- bot/inline.py
+++ bot/inline.py
@@ -1,22 +1,24 @@
 from aiogram import types
 from aiogram.utils.callback_data import CallbackData
 
+
 cb = CallbackData('post', 'action')
 
 
 def get_completed():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     # buttons = ['delete', 'edit']
-    keyboard.add(types.InlineKeyboardButton(text='🗑', callback_data=cb.new('delete')),
-                 types.InlineKeyboardButton(text='📝', callback_data=cb.new('edit')))
+    keyboard.add(types.InlineKeyboardButton(text='🗑', callback_data=cb.new('delete1')),
+                 types.InlineKeyboardButton(text='📝', callback_data=cb.new('edit1')),
+                 types.InlineKeyboardButton(text='✅', callback_data=cb.new('completed')))
     return keyboard
 
 
 def get_inline():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
     # buttons = ['delete', 'edit']
-    keyboard.add(types.InlineKeyboardButton(text='delete', callback_data=cb.new('delete')),
-                 types.InlineKeyboardButton(text='edit', callback_data=cb.new('edit')))
+    keyboard.add(types.InlineKeyboardButton(text='🗑', callback_data=cb.new('delete')),
+                 types.InlineKeyboardButton(text='📝', callback_data=cb.new('edit')))
     return keyboard
 
 
@@ -28,21 +30,30 @@ def get_detail():
 
 def get_detail1():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(types.InlineKeyboardButton(text='detail', callback_data=cb.new('detail1')))
+    keyboard.add(types.InlineKeyboardButton(text='⚙️⚡️⚙️', callback_data=cb.new('detail1')))
     return keyboard
 
 
 def get_detail2():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(types.InlineKeyboardButton(text='detail', callback_data=cb.new('detail2')))
+    keyboard.add(types.InlineKeyboardButton(text='⚙️⚡️⚙️', callback_data=cb.new('detail2')))
     return keyboard
 
 
 def get_edit_buttons():
     keyboard = types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(types.InlineKeyboardButton(text='Task name', callback_data=cb.new('Task name')),
-                 types.InlineKeyboardButton(text='Description', callback_data=cb.new('Description')),
+    keyboard.add(types.InlineKeyboardButton(text='Task name', callback_data=cb.new('task_name')),
+                 types.InlineKeyboardButton(text='Description', callback_data=cb.new('description')),
                  types.InlineKeyboardButton(text='starting_time', callback_data=cb.new('starting_time')),
-                 types.InlineKeyboardButton(text='Deadline', callback_data=cb.new('Deadline'))
+                 types.InlineKeyboardButton(text='Deadline', callback_data=cb.new('deadline')),
+                 types.InlineKeyboardButton(text='Go back', callback_data=cb.new('back'))
                  )
     return keyboard
+
+def calendar():
+    start_kb = types.InlineKeyboardMarkup(row_width=2)
+    start_kb.add(
+                 types.InlineKeyboardButton(text='Navigation Calendar', callback_data=cb.new('Navigation Calendar')),
+                 types.InlineKeyboardButton(text='Dialog Calendar', callback_data=cb.new('Dialog Calendar'))
+                 )
+    return start_kb
